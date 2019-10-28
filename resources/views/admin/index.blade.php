903ffb546eef35a5a872c0823cc1264fc2267b12
Viết Quân <vietquan170497@gmail.com>
2019-10-28 10:21:56 +0700

    update_font_index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -20,7 +20,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     <link href="admin_style/css/style.css" rel='stylesheet' type='text/css' />
     <link href="admin_style/css/style-responsive.css" rel="stylesheet"/>
     <!-- font CSS -->
-    <link href='admin_style///fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
     <!-- font-awesome icons -->
     <link rel="stylesheet" href="admin_style/css/font.css" type="text/css"/>
     <link href="admin_style/css/font-awesome.css" rel="stylesheet">
@@ -135,8 +135,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     <section id="main-content">
         <section class="wrapper">
             @yield('admin_content')
-
-
         </section>
 
         <!-- footer -->
